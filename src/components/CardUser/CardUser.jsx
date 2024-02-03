1d9aad4f3cc7e61e8b21323df9e76a6f670ffa3e
Asker <kotsevasker0@gmail.com>
2024-02-03 10:29:51 +0300

    create section usercard

--- src/components/CardUser/CardUser.jsx
+++ src/components/CardUser/CardUser.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import style from './carduser.module.css'
+import userImage from '../../assets/user.jpeg'
 
 function CardUser() {
+
   return (
-    <div className={style.cardUser}>CardUser</div>
+    <div className={style.cardUser}>
+      <div className={style.hstack}>
+      <div className={style.todos}>
+        <span>Todos</span>
+        <span style={{color:"#C2E88D"}}>+40</span>
+      </div>
+      <div className={style.logo}><img src={userImage} alt="user"/></div>
+      <div className={style.followers}>
+        <span>Follow</span>
+        <span style={{color:"#C2E88D"}}>240</span>
+      </div>
+      </div>
+      <button className={style.btn}>Out</button>
+    </div>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import style from './header.module.css'
-import userIcon from '../../icons/UserIcon/UserIcon.svg'
+import userImage from '../../assets/user.jpeg'
 import {useState} from 'react'
 import  CardUser  from "../CardUser/CardUser.jsx";
 
@@ -19,7 +19,6 @@ function Header() {
   return (
     <div className={style.header}>
        <span className={style.logo}>Todo</span>
-        
         <div className={style.left}>
         <div  className={style.nav}>
         {
@@ -29,7 +28,7 @@ function Header() {
         }
        </div>
        <div onClick={()=>OpenCardUser()}  className={style.user}>
-       <img src={userIcon}/>
+       <img src={userImage}/>
        </div>
        </div>
        <div style={{top:openCard ? "60px" : "-260px" ,opacity:openCard?"1":"0"}} className={style.cardUser}>
